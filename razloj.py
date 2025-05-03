df53864392946f02e88e1ca06995de40ff055d65
DanilBot-ui <grown.dk.up@gmail.com>
2025-05-03 08:41:48 +0300

    v1

---
+++ razloj.py
@@ -0,0 +1,31 @@
+from random import randint
+import uuid
+
+def looptru(listtoloop):
+    questions  = ""
+    for item in listtoloop:
+        questions = questions + "\n" + str(item)
+    return questions
+def generateMathQuestionsRazlojenie():
+    awns = []
+    variant = "Variant:" + str(randint(100000, 999999))
+    qus = [variant]
+
+    for index in range(1, 10):
+        q1 = str(randint(1,10))
+        q2 = str(randint(1, int(q1)))
+        q = q1 + "=" + q2 + "+"
+        evalstr = q1 + "-" + q2
+        awns.append(eval(evalstr))
+        qus.append(q)
+
+    a = input("Need awnser key?(y/n):")
+    b = str(looptru(awns)) + "\n\n" + str(looptru(qus)) if a == "y" else str(looptru(qus))
+    variantUuid =  str(uuid.uuid4())
+    idAwns = variantUuid + "awns.txt"
+    awnserkey = open(idAwns, "w")
+    awnserkey.write(str(looptru(awns)))
+    idQus = variantUuid + "qus.txt"
+    Queskey = open(idQus, "w")
+    Queskey.write(str(looptru(qus)))
+    return b
